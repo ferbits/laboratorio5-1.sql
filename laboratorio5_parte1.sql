00b7c0e5a77f4773c7176680b19042703d766b53
ferbits <oswaldo.arredondo.ttv.783@unilibre.edu.co>
2024-11-07 15:28:59 -0500

    Update laboratorio5_parte1.sql
    Inserción de los datos en la tabla cargo.

--- laboratorio5_parte1.sql
+++ laboratorio5_parte1.sql
@@ -21,3 +21,9 @@ INSERT INTO empleado (id_empleado, nombre_empleado,cargo_empleado, salario_emple
 (1, 'Juan Pérez','Gerente',7500000),
 (2, 'Ana Gómez','Desarrollador',6000000),
 (3, 'Luis Castro','Proyectos',10000000);
+
+USE empresa;
+INSERT INTO cargo (id_cargo, nombre_cargo,descripcion, salario) VALUES 
+(1, 'Gerente','Administrador de la empresa',500000),
+(2, 'Asistante','Personal técnico',250000),
+(3, 'Desarrollador','Gestor de dasarrollo',10000000);
